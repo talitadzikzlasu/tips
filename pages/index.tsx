60ac0a1f3885129f6c40df0ff3dad9c3eea88532
Talita Dzikzlasu <startingcommunity@proton.me>
2023-02-02 23:19:35 +0100

    Add dropshaddow to navbar on scroll

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,24 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.scss";
+import { useEffect, useState } from "react";
+import classnames from "classnames";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    window.onscroll = function () {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+  }, []);
+
   return (
     <>
       <Head>
@@ -15,7 +29,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <nav className={styles.navbar}>
+        <nav
+          className={classnames(
+            styles.navbar,
+            scrolled && styles.navbarShaddow
+          )}
+        >
           <div className={styles.logo}>Calculator </div>
           <div>
             <ul>
